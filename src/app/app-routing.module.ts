d07380a2c8190cec5ad8936a4a8bab04a7368e94
pranav550 <pranavverma550>
2020-05-05 11:23:09 +0530

    remove the comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,3 @@
-import { TableComponent } from './feature/dsm/table/table/table.component';
-import { DashboardComponent } from './core/dashboard/dashboard.component';
 import { DsmComponent } from './feature/dsm/dsm/dsm.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -9,9 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'dsm', pathMatch: 'full' },
   { path: 'dsm', component: DsmComponent },
   { path: 'dsm', loadChildren: () => import(`./feature/dsm/dsm.module`).then(m => m.DsmModule) },
-  { path: 'table', loadChildren: () => import(`./feature/dsm/table/table.module`).then(m => m.TableModule) },
-  // { path: 'table/:id', component: TableComponent },
-  { path: 'column', loadChildren: () => import(`./feature/dsm/table/column/column.module`).then(m => m.ColumnModule) },
+
 ];
 
 @NgModule({
